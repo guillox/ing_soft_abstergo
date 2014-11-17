4d520d2d445716ebfde5c5ba5329b618e3c436da
Silva Agustin <silva.agustinn@gmail.com>
2014-11-17 12:46:45 -0300

    Mejoras de sesion: se agrego remember_me y la barra de arriba funcionan los links de registro y de inicio de sesion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,31 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_filter :require_login, only: [:index, :new, :create]
+  before_filter :require_login, only: [:index]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        redirect_to(:users, notice: 'User was succesfully created!')
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+     redirect_to(:users, notice: 'User was succesfully created!')
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -53,8 +38,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -69,8 +52,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary Internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :last_name, :username, :email, :email_confirmation, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
 	authenticates_with_sorcery!
 
-	validates :password, length: { minimum: 3 }
-	validates :password, confirmation: true
+	validates :name, presence: true
+	validates :last_name, presence: true
+	validates :username, uniqueness: true
+
+	validates :password, length: { minimum: 3 }, confirmation: true
 	validates :password_confirmation, presence: true
 
-	validates :email, uniqueness: true
+	validates :email, uniqueness: true, confirmation: true
+	validates :email_confirmation, presence: true
 end
