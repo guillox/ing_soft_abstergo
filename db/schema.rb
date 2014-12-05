23681da177aad364888fee25c9ca203f53f38361
ArielBujan <arielbujan@hotmail.com>
2014-12-05 04:46:31 -0300

    Historias de Estadisticas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203171815) do
+ActiveRecord::Schema.define(version: 20141205043638) do
 
   create_table "auctions", force: true do |t|
     t.string   "name"
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 20141203171815) do
 
   create_table "searches", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "statistics", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
